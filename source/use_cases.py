52bb62e6e43c3539186dd0096150b3bbfe51880c
Gabriel Farias Caccaos <8305083+gcaccaos@users.noreply.github.com>
2022-02-24 21:37:13 -0300

    feat(usecases): add use cases for card and tag

---
+++ source/use_cases.py
@@ -0,0 +1,51 @@
+"""Application's use cases."""
+from abc import ABC, abstractmethod
+
+from entities import Card, Tag
+
+
+class UseCase(ABC):
+    """Base class for use cases."""
+
+    def __init__(self, repository):
+        self._repository = repository
+
+    @abstractmethod
+    def execute(self):
+        """Implement call to repository method."""
+
+
+class CreateTag(UseCase):
+    """Implements the use of creating a new tag."""
+
+    def execute(self, name):
+        """Create a new tag and persist it in the repository."""
+        tag = Tag(name=name)
+
+        self._repository.save(tag)
+
+
+class ReadTag(UseCase):
+    """Implements the use of creating a new tag."""
+
+    def execute(self, id):  # noqa: VNE003
+        """Return a tag from the repository."""
+        return self._repository.get(id)
+
+
+class CreateCard(UseCase):
+    """Implements the use of creating a new card."""
+
+    def execute(self, text, tags):
+        """Create a new card and persist it in the repository."""
+        card = Card(text=text, tags=tags)
+
+        self._repository.save(card)
+
+
+class ReadCard(UseCase):
+    """Implements the use of reading a card."""
+
+    def execute(self, id):  # noqa: VNE003
+        """Return a card from the repository."""
+        return self._repository.get(id)
---
+++ tests/test_use_cases.py
@@ -0,0 +1,90 @@
+"""Unit tests for the application's use cases."""
+from abc import ABC
+
+from pytest import raises
+from use_cases import CreateCard, CreateTag, ReadCard, ReadTag
+
+
+class BaseTest(ABC):
+    """Base class for use cases' tests."""
+
+    use_case = None
+
+    def has_method(self, name):
+        """Check if it has a specific method."""
+        return callable(getattr(self.use_case, name, None))
+
+    def test_init_without_arguments(self):
+        """Assert that it can't be instantiated without arguments."""
+        with raises(TypeError) as _:
+            self.use_case()
+
+    def test_init_with_arguments(self, mocker):
+        """Assert that it can be instantiated with arguments."""
+        use_case = self.use_case(repository=mocker.Mock())
+
+        assert isinstance(use_case, self.use_case)
+
+    def test_has_execute_method(self):
+        """Assert that it has a method named 'execute'."""
+        assert self.has_method('execute')
+
+
+class TestCreateTag(BaseTest):
+    """Unit tests for the CreateTag use case."""
+
+    use_case = CreateTag
+
+    def test_execute_method_calls_save_method_from_repository(self, mocker):
+        """Assert that it calls the repository's save method."""
+        repository = mocker.Mock()
+
+        use_case = self.use_case(repository=repository)
+        use_case.execute(name='test')
+
+        repository.save.assert_called_once()
+
+
+class TestReadTag(BaseTest):
+    """Unit tests for the ReadTag use case."""
+
+    use_case = ReadTag
+
+    def test_execute_method_calls_get_method_from_repository(self, mocker):
+        """Assert that it calls the repository's get method."""
+        repository = mocker.Mock()
+
+        use_case = self.use_case(repository=repository)
+        use_case.execute(id=None)
+
+        repository.get.assert_called_once()
+
+
+class TestCreateCard(BaseTest):
+    """Unit tests for the CreateCard use case."""
+
+    use_case = CreateCard
+
+    def test_execute_method_calls_save_method_from_repository(self, mocker):
+        """Assert that it calls the repository's save method."""
+        repository = mocker.Mock()
+
+        use_case = self.use_case(repository=repository)
+        use_case.execute(text='test', tags=[])
+
+        repository.save.assert_called_once()
+
+
+class TestReadCard(BaseTest):
+    """Unit tests for the ReadCard use case."""
+
+    use_case = ReadCard
+
+    def test_execute_method_calls_get_method_from_repository(self, mocker):
+        """Assert that it calls the repository's get method."""
+        repository = mocker.Mock()
+
+        use_case = self.use_case(repository=repository)
+        use_case.execute(id=None)
+
+        repository.get.assert_called_once()

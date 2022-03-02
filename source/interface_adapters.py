b4714ea7c3c720e134812604b7c061e435805544
Gabriel Farias Caccaos <8305083+gcaccaos@users.noreply.github.com>
2022-03-01 21:22:27 -0300

    feat(adapters): add in-memory entities repositories
    * tag
    * card

--- source/interface_adapters.py
+++ source/interface_adapters.py
@@ -1,46 +1,93 @@
 """Data access interface adapters for the use cases and frameworks/drivers."""
 from abc import ABC, abstractmethod
 
-
-class TagRepository(ABC):
-    """Base class for tag repository."""
-
-    @abstractmethod
-    def save(self):
-        """Persist a new tag."""
-
-    @abstractmethod
-    def get(self):
-        """Retrieve a tag."""
-
-    @abstractmethod
-    def update(self):
-        """Update a tag and persist the changes."""
-
-    @abstractmethod
-    def remove(self):
-        """Remove a tag and persist the changes."""
+from entities import Card, Entity, Tag
 
 
-class CardRepository(ABC):
-    """Base class for card repository."""
+class Repository(ABC):
+    """Base class for repositories."""
+
+    entity = Entity
 
     @abstractmethod
     def save(self):
-        """Persist a new card."""
+        """Persist a new entity."""
 
     @abstractmethod
     def get(self):
-        """Retrieve a card."""
+        """Retrieve an entity."""
 
     @abstractmethod
     def update(self):
-        """Update a card and persist the changes."""
+        """Update an entity and persist the changes."""
 
     @abstractmethod
     def remove(self):
-        """Remove a card and persist the changes."""
+        """Remove an entity and persist the changes."""
 
     @abstractmethod
     def list(self):
         """Retrieve a list of cards."""
+
+
+class TagRepository(Repository):
+    """Base class for tag repository."""
+
+    entity = Tag
+
+
+class CardRepository(Repository):
+    """Base class for card repository."""
+
+    entity = Card
+
+
+class InMemoryRepository(Repository):
+    """
+    Base class for in-memory repositories.
+
+    The data is stored (privately) as a dictionary.
+    """
+
+    _data = {}
+
+    @classmethod
+    def save(cls, entity):
+        """Persist a new entity."""
+        cls._data[entity.id] = entity
+
+    @classmethod
+    def get(cls, id):  # noqa: VNE003
+        """Retrieve an entity by its id."""
+        return cls._data.get(id)
+
+    @classmethod
+    def update(cls, id, **kwargs):  # noqa: VNE003
+        """Update an entity and persist the changes."""
+        entity = cls.get(id)
+
+        if entity is not None:
+            updated_entity = cls.entity(id=entity.id, **kwargs)
+
+            cls.save(updated_entity)
+
+    @classmethod
+    def remove(cls, id):  # noqa: VNE003
+        """Remove an entity and persist the changes."""
+        entity = cls.get(id)
+
+        if entity is not None:
+            del cls._data[entity.id]
+
+    @classmethod
+    def list(cls):
+        """Retrieve a list of entities."""
+        return list(cls._data.values())
+
+
+class InMemoryTagRepository(InMemoryRepository, TagRepository):
+    """In-memory repository for tags."""
+
+
+class InMemoryCardRepository(InMemoryRepository, CardRepository):
+    """In-memory repository for cards."""
--- tests/test_interface_adapters.py
+++ tests/test_interface_adapters.py
@@ -1,21 +1,21 @@
 """Unit tests for the data access interface adapters."""
 from abc import ABCMeta
 
-from interface_adapters import CardRepository, TagRepository
+from entities import Entity
+from interface_adapters import InMemoryRepository, Repository
+from pytest import fixture
 
 
-class TestTagRepository:
-    """Unit tests for the tag repository."""
-
-    repository = TagRepository
+class TestRepository:
+    """Unit tests for the repository interface adapter."""
 
     def has_method(self, name):
         """Check if it has a specific method."""
-        return callable(getattr(self.repository, name, None))
+        return callable(getattr(Repository, name, None))
 
     def test_is_abstract(self):
         """Assert that it is an abstract base class (ABC)."""
-        assert isinstance(self.repository, ABCMeta)
+        assert isinstance(Repository, ABCMeta)
 
     def test_has_save_method(self):
         """Assert that it has a method named 'save'."""
@@ -23,7 +23,7 @@ class TestTagRepository:
 
     def test_save_method_is_abstract(self):
         """Assert that save is an abstractmethod."""
-        assert self.repository.save.__isabstractmethod__
+        assert Repository.save.__isabstractmethod__
 
     def test_has_get_method(self):
         """Assert that it has a method named 'get'."""
@@ -31,7 +31,7 @@ class TestTagRepository:
 
     def test_get_method_is_abstract(self):
         """Assert that get is an abstractmethod."""
-        assert self.repository.get.__isabstractmethod__
+        assert Repository.get.__isabstractmethod__
 
     def test_has_update_method(self):
         """Assert that it has a method named 'update'."""
@@ -39,7 +39,7 @@ class TestTagRepository:
 
     def test_update_method_is_abstract(self):
         """Assert that update is an abstractmethod."""
-        assert self.repository.update.__isabstractmethod__
+        assert Repository.update.__isabstractmethod__
 
     def test_has_remove_method(self):
         """Assert that it has a method named 'remove'."""
@@ -47,58 +47,49 @@ class TestTagRepository:
 
     def test_remove_method_is_abstract(self):
         """Assert that remove is an abstractmethod."""
-        assert self.repository.remove.__isabstractmethod__
+        assert Repository.remove.__isabstractmethod__
 
 
-class TestCardRepository:
-    """Unit tests for the card repository."""
+class TestInMemoryRepository:
+    """Unit tests for the in-memory repository."""
 
-    repository = CardRepository
+    @fixture
+    def entity(self):
+        """Return an entity."""
+        return Entity()
 
-    def has_method(self, name):
-        """Check if it has a specific method."""
-        return callable(getattr(self.repository, name, None))
+    @fixture
+    def repository(self, entity):
+        """Return the in-memory repository."""
+        repository = InMemoryRepository()
+        repository._data[entity.id] = entity
 
-    def test_is_abstract(self):
-        """Assert that it is an abstract base class (ABC)."""
-        assert isinstance(self.repository, ABCMeta)
+        return repository
 
-    def test_has_save_method(self):
-        """Assert that it has a method named 'save'."""
-        assert self.has_method('save')
+    def test_save_method(self, repository):
+        """Assert that it saves an entity correctly."""
+        entity = Entity()
+        repository.save(entity)
 
-    def test_save_method_is_abstract(self):
-        """Assert that save is an abstractmethod."""
-        assert self.repository.save.__isabstractmethod__
+        assert entity.id in repository._data
 
-    def test_has_get_method(self):
-        """Assert that it has a method named 'get'."""
-        assert self.has_method('get')
+    def test_get_method(self, repository, entity):
+        """Assert that it retrieves an entity correctly."""
+        assert entity == repository.get(entity.id)
 
-    def test_get_method_is_abstract(self):
-        """Assert that get is an abstractmethod."""
-        assert self.repository.get.__isabstractmethod__
+    def test_update_method(self, repository, entity):
+        """Assert that it updates an entity correctly."""
+        kwargs = {}
+        repository.update(id=entity.id, **kwargs)
 
-    def test_has_update_method(self):
-        """Assert that it has a method named 'update'."""
-        assert self.has_method('update')
+        assert entity == repository.get(entity.id)
 
-    def test_update_method_is_abstract(self):
-        """Assert that update is an abstractmethod."""
-        assert self.repository.update.__isabstractmethod__
+    def test_remove_method(self, repository, entity):
+        """Assert that it removes an entity correctly."""
+        repository.remove(entity.id)
 
-    def test_has_remove_method(self):
-        """Assert that it has a method named 'remove'."""
-        assert self.has_method('remove')
+        assert repository.get(entity.id) is None
 
-    def test_remove_method_is_abstract(self):
-        """Assert that remove is an abstractmethod."""
-        assert self.repository.remove.__isabstractmethod__
-
-    def test_has_list_method(self):
-        """Assert that it has a method named 'list'."""
-        assert self.has_method('list')
-
-    def test_list_method_is_abstract(self):
-        """Assert that list is an abstractmethod."""
-        assert self.repository.list.__isabstractmethod__
+    def test_list_method(self, repository):
+        """Assert that it lists all entities correctly."""
+        assert isinstance(repository.list(), list)
